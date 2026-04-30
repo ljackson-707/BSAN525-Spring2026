8722b44f334e7b1dc03bc6a637015d0b86030f6a
ljackson-707 <ljackson@stetson.edu>
2026-04-30 03:01:15 +0000

    Assignment 2 draft

---
+++ Assignment 2/activityrandomizer.py
@@ -0,0 +1,17 @@
+# Import the random module and pandas as pd (pd is the abbreviation for pandas)
+import random   # may not need this library if using pandas functions
+import pandas as pd
+
+# Ask the user to enter today's date for logging and output
+todayDate = input("Enter today's date: ")
+# Ask the user to enter the number of random activities to generate
+actNumber = int(input("Enter the number of activities: "))
+
+# df stands for DataFrame, a pandas function to work with spreadsheets
+# pd.read_excel is pandas function to read from Excel file.
+df = pd.read_excel("activities.xlsx")
+df.sample(n = actNumber)
+
+# Open/initialize output file in write mode. This will be used
+# to write the agenda using the randomized activity list.
+outputFile = open("agenda.txt", "w")
