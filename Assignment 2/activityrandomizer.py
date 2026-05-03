2d8279b3b4c1289c57d8c5d5d57f03c3f312f0fc
ljackson-707 <ljackson@stetson.edu>
2026-05-03 05:20:19 +0000

    updated assignment 2

--- Assignment 2/activityrandomizer.py
+++ Assignment 2/activityrandomizer.py
@@ -10,8 +10,7 @@ Functions used:
 3.
 """
 
-# Import the random module and pandas as pd (pd is the abbreviation for pandas)
-import random   # may not need this library if using pandas functions
+# Import pandas as pd (pd is the abbreviation for pandas)
 import pandas as pd
 
 fileName = input("Enter the file name: ")
@@ -24,14 +23,27 @@ activityNumber = int(input("Enter the number of activities: "))
 # pd.read_excel is pandas function to read from Excel file.
 df = pd.read_excel(fileName)
 randomdf = df.sample(n = activityNumber)  
+activityList = list(randomdf["Name"])
 print(list(randomdf["Name"]))
 
+approve = input("Approve activity list and generate file? (Y/N): ")
+if approve == "Y":
 # Open/initialize output file in write mode. This will be used
 # to write the agenda using the randomized activity list.
-outputFile = open("agenda.txt", "w")
+    outputFile = open("agenda.txt", "w")
+    outputFile.write("Agenda for " + todayDate + "\n\n")
+    for index in range(len(activityList)):      # 4_1c.py example
+        lineNumber = index + 1      # index is zero based, numbering needs to start at 1
+        activity = activityList[index]
+        line = str(lineNumber) + ". " + activity + "\n"     # similar to exercise 4.9
+        outputFile.write(line)
+elif approve == "N":
+    print("The program is closed. No file will be generated.")
+else:
+    print("Invalid input. Please enter Y for Yes or N for No.")
+
 
 # ** To write to an outputfile, outputFile.write(text)
 
 # Close both files at the end of the program, frees up memory
-fileName.close()
 outputFile.close()
