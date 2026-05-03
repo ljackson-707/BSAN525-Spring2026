0bb905057ddf662f228294444f88e67c1947b4c8
ljackson-707 <ljackson@stetson.edu>
2026-05-03 05:47:07 +0000

    added while true loop

--- Assignment 2/activityrandomizer.py
+++ Assignment 2/activityrandomizer.py
@@ -28,23 +28,27 @@ randomdf = df.sample(n = activityNumber)
 activityList = list(randomdf["Name"])
 print(list(randomdf["Name"]))
 
-approve = input("Approve activity list and generate file? (Y/N): ")
-if approve == "Y":
+
+while True:
+    approve = input("Approve activity list and generate file? (Y/N): ")
+    if approve == "Y":
 # Open/initialize output file in write mode. This will be used
 # to write the agenda using the randomized activity list.
-    outputFile = open(outputName, "w")
-    outputFile.write("Agenda for " + todayDate + "\n\n")
-    for index in range(len(activityList)):      # 4_1c.py example
-        lineNumber = index + 1      # index is zero based, numbering needs to start at 1
-        activity = activityList[index]
-        line = str(lineNumber) + ". " + activity + "\n"     # similar to exercise 4.9
-        outputFile.write(line)
-    outputFile.close()
-    print("The agenda has been created in the output text file.")
-elif approve == "N":
-    print("The program is closed. No file will be generated.")
-else:
-    print("Invalid input. Please enter Y for Yes or N for No.")
+        outputFile = open(outputName, "w")
+        outputFile.write("Agenda for " + todayDate + "\n\n")
+        for index in range(len(activityList)):      # 4_1c.py example
+            lineNumber = index + 1      # index is zero based, numbering needs to start at 1
+            activity = activityList[index]
+            line = str(lineNumber) + ". " + activity + "\n"     # similar to exercise 4.9
+            outputFile.write(line)
+        outputFile.close()
+        print("The agenda has been created in the output text file.")
+        break
+    elif approve == "N":
+        print("The program is closed. No file will be generated.")
+        break
+    else:
+        print("Invalid input. Please enter Y for Yes or N for No.")
 
 
 # ** To write to an outputfile, outputFile.write(text)
