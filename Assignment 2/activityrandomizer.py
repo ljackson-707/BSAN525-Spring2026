1051600e1874a54db04bf44aab20d4ac0e94f685
ljackson-707 <ljackson@stetson.edu>
2026-05-03 05:36:00 +0000

    added output file name prompt

--- Assignment 2/activityrandomizer.py
+++ Assignment 2/activityrandomizer.py
@@ -13,11 +13,13 @@ Functions used:
 # Import pandas as pd (pd is the abbreviation for pandas)
 import pandas as pd
 
-fileName = input("Enter the file name: ")
+fileName = input("Enter the Excel file name: ")
 # Ask the user to enter today's date for logging and output
 todayDate = input("Enter today's date: ")
 # Ask the user to enter the number of random activities to generate
 activityNumber = int(input("Enter the number of activities: "))
+# Ask for an output file name.
+outputName = input("Enter the output text file name: ")
 
 # df stands for DataFrame, a pandas function to work with spreadsheets
 # pd.read_excel is pandas function to read from Excel file.
@@ -30,7 +32,7 @@ approve = input("Approve activity list and generate file? (Y/N): ")
 if approve == "Y":
 # Open/initialize output file in write mode. This will be used
 # to write the agenda using the randomized activity list.
-    outputFile = open("agenda.txt", "w")
+    outputFile = open(outputName, "w")
     outputFile.write("Agenda for " + todayDate + "\n\n")
     for index in range(len(activityList)):      # 4_1c.py example
         lineNumber = index + 1      # index is zero based, numbering needs to start at 1
@@ -38,7 +40,7 @@ if approve == "Y":
         line = str(lineNumber) + ". " + activity + "\n"     # similar to exercise 4.9
         outputFile.write(line)
     outputFile.close()
-    print("The agenda has been created in the output file agenda.txt.")
+    print("The agenda has been created in the output text file.")
 elif approve == "N":
     print("The program is closed. No file will be generated.")
 else:
