b135ec5ef137539acb35e7ff76ccb9799f83febc
ljackson-707 <ljackson@stetson.edu>
2026-05-04 02:07:17 +0000

    Added all single line comments

--- Assignment 2/activityrandomizer.py
+++ Assignment 2/activityrandomizer.py
@@ -11,7 +11,7 @@ Functions used:
 4. df.sample() - pandas library function that takes a random sample of rows from the DataFrame (https://pandas.pydata.org/Pandas_Cheat_Sheet.pdf)
 5. list() - Converts the sample from the DataFrame into a list of strings with the activity names (5.1 Lists)
 6. print() - Prints activity list for approval and confirmation prompts to the terminal for user feedback. (1.4 Getting Started with Python Programming)
-7. while True - conditional iteration loop to repeat the prompt until the user enters Y or N to break the loop (3.4 Conditional Iteraion: The while Loop)
+7. while True - conditional iteration loop to repeat the prompt until the user enters Y or N to break the loop (3.4 Conditional Iteration: The while Loop)
 8. if, elif, else - selection statement to perform different functions based on user input and respond with error message if needed (Y/N/other) (3.3 Selection: if and if-else Statements)
 9. for index in range - Loop that repeatedly calls the functions assigned to the loop over the specified range (3.1 Definite Iteration: The for Loop)
 10. range() - used in a count-controlled loop to specify the range is the length of the loop is the length of the activityList (3.1 Definite Iteration: The for Loop)
@@ -45,25 +45,43 @@ activityList = list(randomdf["Name"])
 # Prints the generated activity list to the terminal for user review (Section 1.4)
 print(list(randomdf["Name"]))
 
-# ***START HERE AND CONTINUE WITH SINGLE LINE COMMENTS***
-# Using a while True loop to ensure the user enters a valid Y or N to approve or disapprove of the activity list, or is re-prompted to do so (Section 3.4)
+# Using a while True loop to ensure the user enters a valid Y or N to approve or disapprove 
+# of the activity list, or is re-prompted to do so. If the user inputs Y or N, the loop will 
+# break, or else if there is any other input, the loop continues. (Section 3.4)
 while True:
-    approve = input("Approve activity list and generate file? (Y/N): ")
+# Asks the user to input Y to approve list or N if not (Section 1.4)
+    approve = input("Approve activity list and generate file? Enter Y for Yes or N for No: ")
 # Using an if, elif, else multiway if statement to generate different outputs depending on whether the user inputs Y, N, or something else (Section 3.3)
+# Checks if user input for approve is equal to Y (Section 3.3)
     if approve == "Y":
+# Opens an output file with the outputName in write mode (Section 4.5)
         outputFile = open(outputName, "w")
+# Writes a concatenated string with input from todayDate to the output file (Section 4.5)
         outputFile.write("Agenda for " + todayDate + "\n\n")
-#Using a for loop to 
-        for index in range(len(activityList)):     
-            lineNumber = index + 1      
+# Using a for loop to loop through the range of items in activityList (Section 3.1)
+        for index in range(len(activityList)):
+# Sets lineNumber variable as index + 1 because indexes are zero-based, and the numbered list in the output file needs to start at 1 (Section 4.5)     
+            lineNumber = index + 1
+# Inspects the item in the activity list at the index in the subscript operator (Section 5.1)     
             activity = activityList[index]
-            line = str(lineNumber) + ". " + activity + "\n"     
+# Creates a line for the output file from the lineNumber converted to a string so it can be concatenated with the activity and a newline, so that each activity is printed on a new line (Section 2.2)
+            line = str(lineNumber) + ". " + activity + "\n"
+# Writes the line to an output file (Section 4.5)    
             outputFile.write(line)
+# The for loop has ended and the output file is no longer needed, so this function closes the file (Section 4.5)
         outputFile.close()
+# Prints a confirmation that the output file has been created in the terminal (Section 1.4)
         print("The agenda has been created in the output text file.")
+# Breaks the while True loop since the program has completed (Section 3.4)
         break
+# Checks if user input for approve is equal to N, which means the user did not approve the activity list and program is terminated (Section 3.3)
     elif approve == "N":
+# Prints confirmation to the terminal that the program has ended and no file is generated (Section 1.4)
         print("The program is closed. No file will be generated.")
+# Breaks the while True loop since the program has completed (Section 3.4)        
         break
+# Checks if anything else, other than Y or N was input into the approve prompt (Section 3.3)
     else:
+# Prints an error message to terminal showing that the input was invalid, and asks for proper input (Section 1.4)
         print("Invalid input. Please enter Y for Yes or N for No.")
+# If the while True loop was not broken by a Y or N input, the loop will repeat from the top. 
